d93311b0aa840b354d7b58552bb53b98b21bd6a2
xKaan <kaantopkaya32@gmail.com>
2018-02-13 22:47:12 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,10 +1,30 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 
 var bot = new Discord.Client();
+var prefix = ("!");
 
-bot.on("ready", function() {
+bot.on('ready', function() {
     bot.user.setGame("!help");
     console.log("Le bot a bien été connecté !");
 });
 
 bot.login("NDEzMDM0ODUzMDUxNjYyMzQ2.DWS8QQ.XvBkFLT-J-fs-pAV77Y7UuDinac");
+
+bot.on('message', message => {
+    if (message.content === 'ping'){
+        message.reply('pong');
+        console.log("ping pong");
+    }
+
+    if (message.content === prefix + "help"){
+        var help_embed = new Discord.RichEmbed()
+            .setColor('#121480')
+            .addField("Commandes du bot !", "   !help : Affiche les commandes du bot !")
+            .addField("Interaction", "  ping: Le bot répond pong !"  )
+            .setFooter("Copyright © 2018 PlayThisCube - Tous droits révervés")
+            message.channel.sendEmbed(help_embed)
+        //message.channel.sendMessage("Voici la liste des commands. :\n -!help pour afficher les commandes");
+        console.log("Commande Help demandée !");
+    }
+
+});
