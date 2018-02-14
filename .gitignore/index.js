916298a9980c3ff384d84ab763e4e280230d9806
xKaan <kaantopkaya32@gmail.com>
2018-02-14 15:03:52 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -19,7 +19,7 @@ bot.on('message', message => {
     if (message.content === prefix + "help"){
         var help_embed = new Discord.RichEmbed()
             .setColor('#121480')
-            .addField("Commandes du bot !", "   !help : Affiche les commandes du bot !")
+            .addField("Commandes du bot !", "   !help : Affiche les commandes du bot ! \n!ip : Affiche l'ip du serveur ! \n!site : Affiche le lien du site !")
             .addField("Interaction", "  ping: Le bot répond pong !"  )
             .setFooter("Copyright © 2018 PlayThisCube - Tous droits révervés")
             message.channel.sendEmbed(help_embed)
